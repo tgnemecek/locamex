e2998279f2dae008c2f48b8d2dc5bd93198b0702
Caio Elias <caiom.elias@gmail.com>
2018-05-21 17:19:04 -0300

    - Added dynamic user type dropdown on user registration - Saves user profile info into the database - Added automatic insertion of new userTypes at startup

--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router';
-import { Accounts } from 'meteor/accounts-base';
+import { Accounts } from 'meteor/accounts-password';
 
 import { UserTypes } from '../api/user-types';
 
@@ -16,12 +16,17 @@ export default class Signup extends React.Component {
 
     let email = this.refs.email.value.trim();
     let password = this.refs.password.value.trim();
+    let userProfile = {
+      userName: this.refs.userName.value.trim(),
+      userLastname: this.refs.userLastname.value.trim(),
+      userType: this.refs.userType.value.trim()
+    }
 
     if (password.length < 9) {
       return this.setState({error: 'Password must be more than 8 characters long.'})
     }
 
-    Accounts.createUser({email, password}, (err) => {
+    Accounts.createUser({email, password, userProfile}, (err) => {
       if (err) {
         this.setState({error: err.reason});
       } else {
@@ -29,6 +34,16 @@ export default class Signup extends React.Component {
       }
     });
   }
+
+  listTypes() {
+    let userTypes = UserTypes.find().fetch();
+    return userTypes.map(userType => {
+      return (
+        <option key={userType.type} value={userType.type}>{userType.label}</option>
+      )
+    });
+  }
+
   render() {
     return (
       <div className="boxed-view">
@@ -39,9 +54,9 @@ export default class Signup extends React.Component {
 
           <form onSubmit={this.onSubmit.bind(this)} noValidate className="boxed-view__form">
             <input type="text" ref="userName" name="userName" placeholder="Nome"/>
+            <input type="text" ref="userLastname" name="userLastname" placeholder="Sobrenome"/>
             <select name="userType">
-              <option value="adm">Administrador</option>
-              <option value="guest">Visitante</option>
+              {this.listTypes()}
             </select>
             <input type="email" ref="email" name="email" placeholder="Email"/>
             <input type="password" ref="password" name="password" placeholder="Senha"/>
