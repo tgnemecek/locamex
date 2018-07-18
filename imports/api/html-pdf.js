26878fdd035ffdfcd371f29da199385b6a5ba4ad
thiagonemecek <tgnemecek@gmail.com>
2018-07-18 15:32:36 -0300

    Finished api that creates pdf, sends to client and saves

--- imports/api/html-pdf.js
+++ imports/api/html-pdf.js
@@ -6,18 +6,16 @@ import FileSaver from 'file-saver';
 var pdf = require('html-pdf');
 
 if (Meteor.isClient) {
-  var toBlob = require('stream-to-blob');
-
+  var Buffer = require('buffer/').Buffer;
+  var blob;
   export default function generatePdf() {
     function meteorCall() {
       return new Promise((resolve, reject) => {
         Meteor.call('html-pdf', (err, res) => {
           if (err) reject(err);
           if (res) {
-            toBlob(res, (e, blob) => {
-              console.log('aaa');
-              resolve(blob);
-            })
+            blob = new Blob([res], {type: 'application/pdf'});
+            resolve(blob);
           }
         });
       });
@@ -25,11 +23,8 @@ if (Meteor.isClient) {
     async function asyncCall() {
       console.log('calling');
       var result = await meteorCall();
-      var file = new File(["aaa"], result.filename, {
-  type: "text/plain",
-});
-      // FileSaver.saveAs(file);
-      console.log('file: ', result);
+      FileSaver.saveAs(result, 'contrato.pdf');
+      console.log(result);
     }
     asyncCall();
   }
@@ -37,20 +32,59 @@ if (Meteor.isClient) {
 
 if (Meteor.isServer) {
   var fs = Npm.require('fs');
+  var tmp = require('tmp');
 
   Meteor.methods({
     async 'html-pdf'() {
+      // var a = new Promise((resolve, reject) => {
+      //   setTimeout(() => { resolve(1) }, 3000);
+      // }).then((value) => {
+      //   function c () {
+      //     setTimeout(() => {return value + 1}, 1000)
+      //   };
+      //   return c();
+      // });
+      // await a;
+      // console.log(a);
+      // tentativa 1
+      // var options = { format: 'Letter' };
+      // var html = ReactDOMServer.renderToString(contractShort());
+      // var result = new Promise (resolve => {
+      // pdf.create(html, options).toFile((err, file) => {
+      //     if (err) throw new Error('erro');
+      //     console.log(1, file);
+      //     resolve(file);
+      //   })
+      // }).then((value) => {
+      //   function readFile() {
+      //     fs.readFile(value.filename, (err, data) => {
+      //       if (err) throw new Error('erro');
+      //       console.log(2, data);
+      //       return 'data';
+      //     })
+      //   }
+      //   return readFile();
+      // })
+      // // return await result;
+      // await result;
+      // console.log(3, result);
       var options = { format: 'Letter' };
       var html = ReactDOMServer.renderToString(contractShort());
-      var result = new Promise(resolve => {
-        pdf.create(html, options).toFile((err, file) => {
-          if (err) throw new Error('erro');
-          var stream = fs.createReadStream(file.filename);
-          console.log(stream);
-          resolve(stream);
+      const createFile = () => {
+        return new Promise(resolve => {
+          pdf.create(html, options).toFile((err, file) => {
+            resolve(file);
+          })
         })
-      });
-      return await result;
+      }
+      const readFile = (value) => {
+        return new Promise(resolve => {
+          fs.readFile(value.filename, (err, data) => {
+            resolve(data);
+          })
+        })
+      }
+      return await readFile(await createFile())
     }
   })
 }
