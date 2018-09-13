344da47ea0f70bbde58f5db63d042ecc4fcea446
thiagonemecek <tgnemecek@gmail.com>
2018-09-12 22:41:05 -0300

    Client-side security for RegisterModules

--- imports/api/modules/index.js
+++ imports/api/modules/index.js
@@ -4,86 +4,18 @@ import tools from '/imports/startup/tools/index';
 export const Modules = new Mongo.Collection('modules');
 
 if (Meteor.isServer) {
-
   Meteor.publish('modulesPub', () => {
     return Modules.find();
   })
-
-  // Modules.remove({});
-  // Modules.insert({
-  //   _id: "0000",
-  //   description: "Parede Branca",
-  //   available: 10,
-  //   rented: 5,
-  //   maintenance: 3,
-  //   visible: true
-  // });
-  // Modules.insert({
-  //   _id: "0001",
-  //   description: "Coluna Preta",
-  //   available: 12,
-  //   rented: 10,
-  //   maintenance: 3,
-  //   visible: true
-  // });
-  // Modules.insert({
-  //   _id: "0002",
-  //   description: "Coluna Galvanizada",
-  //   available: 132,
-  //   rented: 100,
-  //   maintenance: 3,
-  //   visible: true
-  // });
-  // Modules.insert({
-  //   _id: "0003",
-  //   description: "Coluna Azul",
-  //   available: 132,
-  //   rented: 100,
-  //   maintenance: 7,
-  //   visible: true
-  // });
-  // Modules.insert({
-  //   _id: "0004",
-  //   description: "Piso Preto",
-  //   available: 132,
-  //   rented: 100,
-  //   maintenance: 10,
-  //   visible: true
-  // });
-  // Modules.insert({
-  //   _id: "0005",
-  //   description: "Piso Branco",
-  //   available: 132,
-  //   rented: 100,
-  //   maintenance: 35,
-  //   visible: true
-  // });
-  // Modules.insert({
-  //   _id: "0006",
-  //   description: "Teto Preto",
-  //   available: 132,
-  //   rented: 100,
-  //   maintenance: 1,
-  //   visible: true
-  // });
-  // Modules.insert({
-  //   _id: "0007",
-  //   description: "Teto Azul",
-  //   available: 132,
-  //   rented: 100,
-  //   maintenance: 0,
-  //   visible: true
-  // });
-
   Meteor.methods({
     'modules.insert'(state) {
       const _id = tools.generateId(Modules);
       const data = {
         _id,
         description: state.description,
-        available: state.available,
-        rented: state.rented,
-        maintenance: state.maintenance,
+        available: state.available || 0,
+        rented: state.rented || 0,
+        maintenance: state.maintenance || 0,
         visible: true
       };
       Modules.insert(data);
@@ -134,9 +66,9 @@ if (Meteor.isServer) {
       const data = {
         _id: state._id,
         description: state.description,
-        available: state.available,
-        rented: state.rented,
-        maintenance: state.maintenance,
+        available: state.available || 0,
+        rented: state.rented || 0,
+        maintenance: state.maintenance || 0,
         visible: true
       };
       Modules.update({ _id: state._id }, { $set: data });
--- imports/components/RegisterModules/index.js
+++ imports/components/RegisterModules/index.js
@@ -23,6 +23,9 @@ export default class RegisterModules extends React.Component {
       transaction: 0,
       destination: '-',
 
+      errorMsg: '',
+      errorKeys: [],
+
       confirmationWindow: false
     }
   }
@@ -38,72 +41,77 @@ export default class RegisterModules extends React.Component {
     this.props.toggleWindow();
   }
   saveEdits = () => {
-    const transaction = () => {
-      if (this.state.transaction === 0) return;
-      if (this.state.origin === '-' && this.state.destination === '-') return;
-      if (this.state.origin === '-') {
-        this.state[this.state.destination] += this.state.transaction;
-        return;
-      }
-      if (this.state.destination === '-') {
-        this.state[this.state.origin] -= this.state.transaction;
-        return;
-      }
-      this.state[this.state.origin] -= this.state.transaction;
-      this.state[this.state.destination] += this.state.transaction;
+    var errorKeys = [];
+    function transaction (originalState) {
+      var state = { ...originalState };
+      if (originalState.transaction === 0) return originalState;
+      if (originalState.origin === '-' && originalState.destination === '-') return originalState;
+
+      var from = originalState.origin;
+      var transactionValue = Number(originalState.transaction);
+      var to = originalState.destination;
+
+      if (from !== '-') state[from] = Number (state[from]) - transactionValue;
+      if (to !== '-') state[to] = Number (state[to]) + transactionValue;
+
+      return state;
     }
-    transaction();
-    if (this.props.item._id) {
-      Meteor.call('modules.update', this.state);
-    } else Meteor.call('modules.insert', this.state);
-    this.props.toggleWindow();
+    if (!this.state.description.trim()) {
+      errorKeys.push("description");
+      this.setState({ errorMsg: "Favor informar uma descrição.", errorKeys });
+    } else {
+      var state = transaction(this.state);
+      if (this.props.item._id) {
+        Meteor.call('modules.update', state);
+      } else Meteor.call('modules.insert', state);
+      this.props.toggleWindow();
+    }
   }
   render() {
     return (
-      <ErrorBoundary>
-        <Box
-          title={this.props.item._id ? "Editar Componente" : "Criar Novo Componente"}
-          closeBox={this.props.toggleWindow}
-          width="800px">
-            <Block columns={6} options={[{block: 1, span: 5}]}>
-              <Input
-                title="Código:"
-                type="text"
-                readOnly={true}
-                name="_id"
-                value={this.state._id}
-                onChange={this.onChange}
-              />
-              <Input
-                title="Descrição:"
-                type="text"
-                name="description"
-                value={this.state.description}
-                onChange={this.onChange}
-              />
-            </Block>
-            <h4 className="register-modules__transaction__title">Movimentação de estoque:</h4>
-            <Transaction item={this.state} onChange={this.onChange}/>
-            {this.state.confirmationWindow ?
-              <Box
-                title="Aviso:"
-                closeBox={this.toggleConfirmationWindow}>
-                <p>Deseja mesmo excluir este item do banco de dados?</p>
-                <FooterButtons buttons={[
-                  {text: "Não", className: "button--secondary", onClick: () => this.toggleConfirmationWindow()},
-                  {text: "Sim", className: "button--danger", onClick: () => this.removeItem()}
-                ]}/>
-              </Box>
-            : null}
-            {this.props.item._id ?
-              <button className="button button--danger" style={{width: "100%"}} onClick={this.toggleConfirmationWindow}>Excluir Registro</button>
-            : null}
-            <FooterButtons buttons={[
-              {text: "Voltar", className: "button--secondary", onClick: () => this.props.toggleWindow()},
-              {text: "Salvar", onClick: () => this.saveEdits()}
-            ]}/>
-        </Box>
-      </ErrorBoundary>
+      <Box
+        title={this.props.item._id ? "Editar Componente" : "Criar Novo Componente"}
+        closeBox={this.props.toggleWindow}
+        width="800px">
+          <div className="error-message">{this.state.errorMsg}</div>
+          <Block columns={6} options={[{block: 1, span: 5}]}>
+            <Input
+              title="Código:"
+              type="text"
+              readOnly={true}
+              name="_id"
+              value={this.state._id}
+              onChange={this.onChange}
+            />
+            <Input
+              title="Descrição:"
+              type="text"
+              name="description"
+              value={this.state.description}
+              onChange={this.onChange}
+            />
+          </Block>
+          <h4 className="register-modules__transaction__title">Movimentação de estoque:</h4>
+          <Transaction item={this.state} onChange={this.onChange}/>
+          {this.state.confirmationWindow ?
+            <Box
+              title="Aviso:"
+              closeBox={this.toggleConfirmationWindow}>
+              <p>Deseja mesmo excluir este item do banco de dados?</p>
+              <FooterButtons buttons={[
+                {text: "Não", className: "button--secondary", onClick: () => this.toggleConfirmationWindow()},
+                {text: "Sim", className: "button--danger", onClick: () => this.removeItem()}
+              ]}/>
+            </Box>
+          : null}
+          {this.props.item._id ?
+            <button className="button button--danger" style={{width: "100%"}} onClick={this.toggleConfirmationWindow}>Excluir Registro</button>
+          : null}
+          <FooterButtons buttons={[
+            {text: "Voltar", className: "button--secondary", onClick: () => this.props.toggleWindow()},
+            {text: "Salvar", onClick: () => this.saveEdits()}
+          ]}/>
+      </Box>
     )
   }
 }
