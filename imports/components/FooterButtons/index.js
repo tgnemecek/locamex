e7e13b041210e3e3265ca9d6daced15deaaf8937
thiagonemecek <tgnemecek@gmail.com>
2019-01-14 13:24:35 -0200

    WIP

--- imports/components/FooterButtons/index.js
+++ imports/components/FooterButtons/index.js
@@ -6,6 +6,8 @@ export default class FooterButtons extends React.Component {
   selectNumber = () => {
     var length = this.props.buttons.length;
     switch (length) {
+      case 1:
+        return this.renderOneButton();
       case 2:
         return this.renderTwoButtons();
       case 3:
@@ -13,6 +15,24 @@ export default class FooterButtons extends React.Component {
       default:
         return null;
     }
+  }
+
+  renderOneButton = () => {
+    var style = {flexBasis: "100%"};
+    var button = this.props.buttons[0];
+
+    if (!button) return null;
+
+    var className = "button ";
+    className += button.className || '';
+    var text = button.text;
+    var onClick = button.onClick || null;
+    var type = button.type || "button";
+    return <button
+                type={type}
+                style={style}
+                className={className}
+                onClick={onClick}>{text}</button>
   }
 
   renderTwoButtons = () => {
