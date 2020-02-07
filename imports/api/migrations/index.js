352538cc59977e2a61908e6c69bcf49eb3f13861
thiagonemecek <tgnemecek@gmail.com>
2020-02-06 19:31:16 -0500

    WIP

--- imports/api/migrations/index.js
+++ imports/api/migrations/index.js
@@ -15,69 +15,69 @@ import { Services } from '/imports/api/services/index';
 import { Series } from '/imports/api/series/index';
 
 if (Meteor.isServer) {
-  Migrations.add({
-    version: 2,
-    name: "Adapt Users to SimpleSchema",
-    up: function() {
-      var users = Meteor.users.find({}).fetch();
-      users.forEach((user) => {
-        var _id = user._id;
-        var newUser = {
-          ...user,
-          profile: {
-            firstName: user.firstName,
-            lastName: user.lastName,
-            type: user.type
-          }
-        }
-        delete newUser.firstName;
-        delete newUser.lastName;
-        delete newUser.type;
-        delete newUser.pages;
-        delete newUser._id;
-
-        schema('users', 'insert').validate(newUser);
-
-        Meteor.users.update({ _id }, newUser);
-      })
-    },
-    down: function() {}
-  });
-  Migrations.add({
-    version: 1,
-    name: "Adapt Accessories to SimpleSchema",
-    up: function() {
-      Accessories.update(
-        {},
-        {
-          $unset: {place: "", rented: ""}
-        },
-        {multi: true}
-      )
-      Accessories.update(
-        {images: {$exists: false}},
-        {
-          $set: {images: []}
-        },
-        {multi: true}
-      )
-      var acc = Accessories.find().fetch();
-      acc.forEach((item) => {
-        item.variations.forEach((variation, i, arr) => {
-          if (!variation._id) {
-            variation._id = tools.generateId();
-          }
-          var description = "Padrão Único";
-          if (arr.length > 1) {
-            description = "Padrão " + tools.convertToLetter(i);
-          }
-          variation.description = description;
-        })
-        var _id = item._id;
-        delete item._id;
-        Accessories.update({_id}, item);
-      })
-    },
-    down: function() {}
-  });
+  // Migrations.add({
+  //   version: 2,
+  //   name: "Adapt Users to SimpleSchema",
+  //   up: function() {
+  //     var users = Meteor.users.find({}).fetch();
+  //     users.forEach((user) => {
+  //       var _id = user._id;
+  //       var newUser = {
+  //         ...user,
+  //         profile: {
+  //           firstName: user.firstName,
+  //           lastName: user.lastName,
+  //           type: user.type
+  //         }
+  //       }
+  //       delete newUser.firstName;
+  //       delete newUser.lastName;
+  //       delete newUser.type;
+  //       delete newUser.pages;
+  //       delete newUser._id;
+  //
+  //       schema('users', 'insert').validate(newUser);
+  //
+  //       Meteor.users.update({ _id }, newUser);
+  //     })
+  //   },
+  //   down: function() {}
+  // });
+  // Migrations.add({
+  //   version: 1,
+  //   name: "Adapt Accessories to SimpleSchema",
+  //   up: function() {
+  //     Accessories.update(
+  //       {},
+  //       {
+  //         $unset: {place: "", rented: ""}
+  //       },
+  //       {multi: true}
+  //     )
+  //     Accessories.update(
+  //       {images: {$exists: false}},
+  //       {
+  //         $set: {images: []}
+  //       },
+  //       {multi: true}
+  //     )
+  //     var acc = Accessories.find().fetch();
+  //     acc.forEach((item) => {
+  //       item.variations.forEach((variation, i, arr) => {
+  //         if (!variation._id) {
+  //           variation._id = tools.generateId();
+  //         }
+  //         var description = "Padrão Único";
+  //         if (arr.length > 1) {
+  //           description = "Padrão " + tools.convertToLetter(i);
+  //         }
+  //         variation.description = description;
+  //       })
+  //       var _id = item._id;
+  //       delete item._id;
+  //       Accessories.update({_id}, item);
+  //     })
+  //   },
+  //   down: function() {}
+  // });
 }
