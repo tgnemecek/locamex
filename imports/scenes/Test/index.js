a8b2bf4c366569386ce26e958e25c6d3cdf6a472
thiagonemecek <tgnemecek@gmail.com>
2020-02-17 18:34:04 -0500

    WIP

--- imports/scenes/Test/index.js
+++ imports/scenes/Test/index.js
@@ -16,13 +16,24 @@ class TestPage extends React.Component {
   render() {
     return (
       <div>
-        {this.props.array.map((item) => {
-          return item.places.map((item1) => {
-            return (
-              <p>{item1}</p>
-            )
-          })
-        })}
+        <div className="row">
+          <div>Data</div>
+          <div>Tipo</div>
+          <div>Qtd.</div>
+          <div>Produto</div>
+          <div>Série/Variação</div>
+          <div>Origem/Destino</div>
+          <div></div>
+        </div>
+        <div className="row">
+          <div>17-02-2020</div>
+          <div>Envio</div>
+          <div>1</div>
+          <div>Loca 300 M</div>
+          <div>M0014</div>
+          <div>Antonio Pepe 52</div>
+          <div>P</div>
+        </div>
       </div>
     )
   }
