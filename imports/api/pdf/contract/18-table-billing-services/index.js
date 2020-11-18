7d9a556302de46fadb0febfb5bf05e22c1420d9f
thiagonemecek <tgnemecek@gmail.com>
2020-11-18 11:51:34 -0500

    Fix wrong taxes in pdf contract

--- imports/api/pdf/contract/18-table-billing-services/index.js
+++ imports/api/pdf/contract/18-table-billing-services/index.js
@@ -1,33 +1,52 @@
-import tools from '/imports/startup/tools/index';
-import moment from 'moment';
+import tools from "/imports/startup/tools/index";
+import moment from "moment";
 
 export default function tableBillingServices(props) {
   var accountServices = props.billingServices[0].account;
-  var accountBaseText = ` - Informamos: Pagamentos de Notas Fiscais Eletrônicas (NFe) são exclusivos através de Depósito Bancário junto ao Banco ${accountServices.bank} (${accountServices.bankNumber}) Agência ${accountServices.branch} C/C ${accountServices.number} a favor da LOCADORA.`
+  var accountBaseText = ` - Informamos: Pagamentos de Notas Fiscais Eletrônicas (NFe) são exclusivos através de Depósito Bancário junto ao Banco ${accountServices.bank} (${accountServices.bankNumber}) Agência ${accountServices.branch} C/C ${accountServices.number} a favor da LOCADORA.`;
 
   const body = () => {
     return props.billingServices.map((charge, i, array) => {
-      var index = (i + 1);
+      var index = i + 1;
       var expiryDate = moment(charge.expiryDate).format("DD/MM/YYYY");
       var description = charge.description + accountBaseText;
-      var taxes = {text: tools.format((charge.value * (charge.inss + charge.iss) / 100), 'currency'), alignment: 'center'};
-      var value = {text: tools.format(charge.value, 'currency'), alignment: 'right'};
+      var taxes = {
+        text: tools.format(
+          charge.value * (charge.inss + charge.iss),
+          "currency"
+        ),
+        alignment: "center",
+      };
+      var value = {
+        text: tools.format(charge.value, "currency"),
+        alignment: "right",
+      };
       return [index, expiryDate, description, taxes, value];
     });
-  }
+  };
   return props.generateTable({
-    header: [[
-      '#',
-      'Vencimento',
-      'Descrição da Cobrança',
-      {text: 'INSS + ISS', alignment: 'center'},
-      {text: 'Valor Final', alignment: 'right'}
-    ]],
+    header: [
+      [
+        "#",
+        "Vencimento",
+        "Descrição da Cobrança",
+        { text: "INSS + ISS", alignment: "center" },
+        { text: "Valor Final", alignment: "right" },
+      ],
+    ],
     body: body(),
-    footer: [[
-      {text: 'Valor Total do Pacote de Serviços:', colSpan: 4, alignment: 'right', bold: true}, props.resultFormat(props.totalValueServices)
-    ]],
-    widths: ['auto', 'auto', '*', 50, 60],
-    styles: props.styles
-  })
+    footer: [
+      [
+        {
+          text: "Valor Total do Pacote de Serviços:",
+          colSpan: 4,
+          alignment: "right",
+          bold: true,
+        },
+        props.resultFormat(props.totalValueServices),
+      ],
+    ],
+    widths: ["auto", "auto", "*", 50, 60],
+    styles: props.styles,
+  });
 }
